274b98b9232e2b4fee7993d34e511d183444e784
wzsamuels <wzsamuels@gmail.com>
2022-06-22 16:50:57 -0400

    Removed unused pages and apis.

--- pages/portfolio.js
+++
@@ -1,9 +0,0 @@
-const Portfolio = () => {
-  return (
-    <iframe width="1920" height="1080"
-            src="https://xd.adobe.com/embed/b50f1917-5f17-404f-b8d3-57b3bf21200c-e05c/?fullscreen" frameBorder="0"
-            allowFullScreen></iframe>
-  )
-}
-
-export default Portfolio
