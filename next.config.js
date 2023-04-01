f49a924bc609891ae2560e3c4fdddafd8ac781ee
wzsamuels <wzsamuels@gmail.com>
2023-03-31 20:17:15 -0400

    Add PWA support to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'upcity-marketplace.s3.amazonaws.com'],
   },
-}
+})
 
 module.exports = nextConfig
